bf3e96ffcfd9b4e597f154cc67982c586269eccd
Alin Florin <alin@qoffice.ro>
2023-06-02 17:54:46 +0300

    -

--- customize_at_boot.sh
+++ customize_at_boot.sh
@@ -25,48 +25,6 @@ else
 
     mkdir -p $HOME/.local/share/applications
 
-
-    # Brave
-    if [ ! -d "$HOME/Software/brave" ] 
-    then
-        echo "Installing Brave..."
-        cd $HOME/Software
-        mkdir -p $HOME/Software/brave
-        cd $HOME/Software/brave
-        wget https://github.com/brave/brave-browser/releases/download/v1.46.153/brave-browser-1.46.153-linux-$(dpkg --print-architecture).zip
-        unzip ./brave-browser-1.46.153-linux-$(dpkg --print-architecture).zip
-        rm -rf ./brave-browser-1.46.153-linux-$(dpkg --print-architecture).zip
-        chmod +x ./brave
-        chmod +x ./brave-browser
-        ln -s $HOME/Software/brave/brave-browser $HOME/Software/brave-browser
-        touch $HOME/.local/share/applications/brave.desktop
-        tee -a $HOME/.local/share/applications/brave.desktop << END
-[Desktop Entry]
-Type=Application
-Icon=/home/kasm-user/Software/brave/product_logo_64.png
-Name=Brave
-Comment=Brave Browser
-Categories=Network;
-Exec=/home/kasm-user/Software/brave/brave-browser --no-sandbox
-Path=/home/kasm-user/Software/brave
-StartupNotify=true
-Terminal=false
-END
-        touch $HOME/.config/mimeapps.list
-        tee -a $HOME/.config/mimeapps.list << END
-[Default Applications]
-x-scheme-handler/http=brave.desktop
-x-scheme-handler/https=brave.desktop
-END
-        touch $HOME/Software/brave-browser-ns
-        tee -a $HOME/Software/brave-browser-ns << END
-#!/bin/bash
-brave-browser --no-sandbox
-END
-        chmod +x $HOME/Software/brave-browser-ns
-        grep -qxF 'export BROWSER=brave-browser-ns' $HOME/.bashrc || echo 'export BROWSER=brave-browser-ns' >> $HOME/.bashrc
-    fi
-
     
     # kubectl
     if [ ! -f "$HOME/Software/kubectl" ] 
@@ -86,25 +44,6 @@ END
         rm -rf ./linux-$(dpkg --print-architecture)
         rm -rf ./helm-v3.12.0-linux-$(dpkg --print-architecture).tar.gz
         chmod +x ./helm
-    fi
-
-    # k3s
-    if [ ! -f "$HOME/Software/k3s" ]
-    then
-        cd $HOME/Software
-        mkdir -p $HOME/.rancher/k3s
-        mkdir -p $HOME/.kube
-        arch=$(dpkg --print-architecture)
-        if [ "$arch" = "amd64" ]; then
-            curl -L --output k3s "https://github.com/k3s-io/k3s/releases/download/v1.27.1%2Bk3s1/k3s"
-        else
-            curl -L --output k3s "https://github.com/k3s-io/k3s/releases/download/v1.27.1%2Bk3s1/k3s-$(dpkg --print-architecture)"
-        fi
-        
-        chmod +x ./k3s
-        echo '#!/bin/bash' > $HOME/Desktop/start_k3s.sh
-        echo 'sudo k3s server --snapshotter native --data-dir /home/kasm-user/.rancher/k3s --disable traefik --write-kubeconfig-mode "0777" --write-kubeconfig /home/kasm-user/.kube/config' >> $HOME/Desktop/start_k3s.sh
-        chmod +x $HOME/Desktop/start_k3s.sh
     fi
 
     # telepresence
